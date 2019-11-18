a160563f836a9469877a53d4a39d5e7f38c5ea27
Manosh <manosh@bs-23.net>
2019-11-18 17:39:24 +0600

    Add lead edit and POST routes

--- controllers/landing.js
+++ controllers/landing.js
@@ -27,7 +27,27 @@ module.exports = {
                 id: req.params.lead_id
             }
         }).then(lead => {
-            res.render('lead', {  lead: lead});
+            res.render('lead/lead', {  lead: lead});
+        });
+    },
+    show_edit_lead: (req, res, next) => {
+        return models.Lead.findOne({
+            where: {
+                id: req.params.lead_id
+            }
+        }).then(lead => {
+            res.render('lead/edit_lead', { lead: lead });
+        });
+    },
+    edit_lead: (req, res, next) => {
+        return models.Lead.update({
+            email: req.body.lead_email
+        }, {
+            where: {
+                id: req.params.lead_id
+            }
+        }).then(result => {
+            res.redirect('/lead/' + req.params.lead_id);
         });
     }
 };
